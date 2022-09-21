b8b4359a8c961478c95801a0002cdf8ba4edf62f
Saiful Rijal <46024788+saifulrijal-ds@users.noreply.github.com>
2022-09-21 11:54:33 +0700

    build and run test docker image

---
+++ pipeline.py
@@ -0,0 +1,11 @@
+import sys
+import pandas as pd
+
+# sys.argv[0] # is the name of file
+print(sys.argv)
+
+day = sys.argv[1]
+
+# some fancy stuff with pandas
+
+print(f'job finish successfully for day = {day}')
